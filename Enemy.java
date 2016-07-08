7171c4f89551594874321d79b4190e85de5c6b98
Kenny Vu <Kenny Vu>
2016-07-08 13:19:51 +0100

    New Feature: Created subclasses of Unit; Hero and Enemy

---
+++ Enemy.java
@@ -0,0 +1,9 @@
+public class Enemy extends Unit
+{
+    public Enemy(int hp, int atk, int spd)
+    {
+        super(hp,atk,spd);
+        setUnitImage("images/enemy_image.png");
+    }
+    
+}
---
+++ Hero.java
@@ -0,0 +1,7 @@
+public class Hero extends Unit
+{
+    public Hero(int hp, int atk, int spd){
+        super(hp,atk,spd);
+        setUnitImage("images/hero_image.png");
+    }
+}
